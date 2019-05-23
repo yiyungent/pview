da9cb524d283dc66c8e51daedff0d6a623c1ee21
yiyun <yiyungent@gmail.com>
2019-05-23 08:32:37 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+<p align="center">
+<img src="_images/PView.png" alt="PView">
+</p>
+<h1 align="center">PView</h1>
+
+> 致敬PJAX
+
+[![repo size](https://img.shields.io/github/repo-size/yiyungent/pview.svg?style=flat)]()
+[![LICENSE](https://img.shields.io/github/license/yiyungent/pview.svg?style=flat)](https://mit-license.org/)
+
+
+## 介绍
+
+致敬 PJAX
---
+++ src/pview.js
@@ -0,0 +1,92 @@
+﻿function PView(options) {
+
+	this.init = function () {
+		var that = this;
+		$('[pview-btn]').on('click', function (e) {
+			console.log('init start');
+			// 阻止默认事件
+			e.preventDefault();
+			var goUrl = '';
+			if (this.hasAttribute('pview-url')) {
+				goUrl = this.getAttribute('pview-url');
+			} else if (this.tagName.toLowerCase() == 'a') {
+				goUrl = this.getAttribute('href');
+			} else {
+				goUrl = window.location.href;
+			}
+
+			var pview = ''
+			if (this.hasAttribute('pview-targets')) {
+				pview = this.getAttribute('pview-targets');
+			}
+
+			that.go(pview, goUrl, 'get', {});
+			console.log('init success');
+		});
+
+		window.addEventListener('popstate', function(event) {
+			console.log('location: ' + document.location);
+			console.log(event.state);
+		});
+	},
+
+	/**
+	 * 前往此页面
+	 * @param {String} pview 指定要更新的区块 eg: top-nav,main-content
+	 * @param {String} url	eg: www.baidu.com
+	 * @param {String} type eg: get or post
+	 * @param {String | Object} data 要发送的数据
+	 */
+	this.go = function (pview, url, type, data) {
+		$.ajax({
+			url: url,
+			type: type,
+			headers: { "pview": pview },
+			data: data,
+			dataType: 'html',
+			success: function (data) {
+				// 改页面 url
+				if (!!(window.history && history.pushState)) {
+					history.pushState(null, '', url);
+				}
+
+				// html字符串转dom对象
+				var dataObj = document.createElement("code");
+				dataObj.innerHTML = data;
+
+				// 需要更新的区块
+				var updatePviews = [];
+				var updatePviewNames = [];
+				if (pview != null && pview.trim() != '') {
+					updatePviewNames = pview.split(',');
+				}
+				for (var i = 0; i < updatePviewNames.length; i++) {
+					// updatePviews[i] = $('[pview="' + updatePviewNames[i] + '"]')[0]; // 注意：每个是一个 js对象
+					updatePviews[i] = document.querySelector('[pview="' + updatePviewNames[i] + '"]');
+				}
+				// 查找当前页面的（点击要求更新的）每一个 区块, 并将返回的 html 从中筛选出 相应区块，将对应的 旧的 区块替换
+				console.log('---------updatePviews-----------');
+				console.log(updatePviews);
+				var pviewItem = null;
+				var pviewItemName = '', pviewItemHtml = '';
+				for (var i = 0; i < updatePviews.length; i++) {
+					// 当前 pview块的 名
+					// pviewItemName = $(updatePviews[i]).attr('pview');
+					pviewItemName = updatePviews[i].getAttribute('pview');
+					// 当前 pview块的 新返回的 区块
+					pviewItem = dataObj.querySelector('[pview="' + pviewItemName + '"]');
+					if (pviewItem != null) {
+						// 返回的 html 中存在此区块，才更新此要求的区块
+						pviewItemHtml = pviewItem.innerHTML;
+						console.log('---------updatePviewItem-----------');
+						console.log(updatePviews[i]);
+						updatePviews[i].innerHTML = pviewItemHtml;
+						console.log('----update success-----');
+					}
+				}
+			}
+		});
+	}
+
+
+}
